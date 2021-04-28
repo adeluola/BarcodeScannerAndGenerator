7f6ae3fd11cf7597b4bea354751ec185dc71d6b2
Adeluola <bimbodelu@gmail.com>
2021-04-28 13:52:31 +0100

    Barcode scanner and Barcode Generator withot css  styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BarcodeFormat} from '@zxing/library';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  resString: string | undefined;
+  value = '';
+
   title = 'scanner-app';
+  allowedFormats = [ BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX /*, ...*/ ];
+  onCode(resultString: string): void
+  {
+    this.resString = resultString;
+  }
+   generate(inputed: string): void
+   {
+    this.value = inputed;
+   }
+
 }
